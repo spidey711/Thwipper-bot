62107ec4c4d46fd66f33f4a4164bd224c70bee54
TamonudSharma <73441372+spidey711@users.noreply.github.com>
2021-08-18 23:49:44 +0530

    updated required modules

---
+++ modules.py
@@ -0,0 +1,22 @@
+import discord
+from discord.utils import get
+from discord.ext import commands, tasks
+from links import *
+from important import token, sql_pass
+import mysql.connector as ms
+import os
+import sys
+import random
+import calendar
+import pytz
+import datetime
+import regex
+import pytube
+import ffmpeg
+import asyncio
+import requests
+import wikipedia
+import youtube_dl 
+import urllib.request
+from googlesearch import search
+from cryptography.fernet import Fernet

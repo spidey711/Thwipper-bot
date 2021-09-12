bc56216dd3c2902d5bbf148393b4dcb113e93af0
TamonudSharma <73441372+spidey711@users.noreply.github.com>
2021-09-12 11:11:15 +0530

    added responses for commands

--- responses.py
+++ responses.py
@@ -1,1 +1,11 @@
-# Going to add responses here...
+# QUIPS
+titles = ["Quip Incoming","Here you go!","A quip coming right up!"]
+footers = ["Really interesting if I do say so myself.","Gotta say, this still holds up to today's standards.","Damn, I remember this like it was yesterday.","Holy shit, that was a good!","Man, some of these quips are, in fact, fairly rad!","Did I mention I was present during that time?"]
+
+# Profile Picture
+compliments = ["Man, the daily bugle would pay a lot for this 🤩", "This is a nice one! I like it 😎", "Oh Boy! JJJ is gonna be real happy 😃", "The profile picture is fire 🔥🔥🔥","Great Profile Picture, if I do say so myself 🤔","Damn, where'd you get this? 💙", "Solid 8/10 😎","Beautiful 🌃", "Solid 9/10 😎", "I will consider this an exception and give you 10/10 😎","Your FBI Agent will be proud of this profile picture 👌🏻"]
+
+# Shells
+denied = ["os.system('start cmd')",'os.system("start cmd")',"sys.exit()", "eval(sys.exit)", "token", "sql_pass", 'eval("token")', "eval('token')",'eval("sql_pass")',"key","cipher","eval('cipher')",'eval("cipher")',"eval('key')",'eval("key")',"reddit_client_id","eval('reddit_client_id')",'eval("reddit_client_id")',"reddit_client_secret","eval('reddit_client_secret')",'eval("reddit_client_secret")',"reddit_user_agent","eval('reddit_user_agent')",'eval("reddit_user_agent")',"reddit_client_id","eval('reddit_client_id')",'eval ("reddit_client_id")',"reddit_user_pass","eval('reddit_user_pass')",'eval("reddit_user_pass")',"reddit_client_id","eval('reddit_client_id')",'eval("reddit_client_id")',"10**10**10"]
+denied_responses = ["Uh uh, no can do man. That function will not be executed.","Nope! Can't and won't execute that function","Haha, sorry I cannot possibly execute that function.","That function cannot be executed."]
+no_functions = ["Functions will not be executed. Try w/o brackets to get information.","No functions allowed. Please enter w/o brackets to get information.","I cannot execute functions, try entering w/o brackets to get information."]

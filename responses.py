555db9e3942b846e89346e11459b00ac3ffbccd1
TamonudSharma <73441372+spidey711@users.noreply.github.com>
2021-09-14 20:45:11 +0530

    removed on_Message responses

--- responses.py
+++ responses.py
@@ -5,9 +5,6 @@ footers = ["Really interesting if I do say so myself.","Gotta say, this still ho
 # Profile Picture
 compliments = ["Man, the daily bugle would pay a lot for this 🤩", "This is a nice one! I like it 😎", "Oh Boy! JJJ is gonna be real happy 😃", "The profile picture is fire 🔥🔥🔥","Great Profile Picture, if I do say so myself 🤔","Damn, where'd you get this? 💙", "Solid 8/10 😎","Beautiful 🌃", "Solid 9/10 😎", "I will consider this an exception and give you 10/10 😎","Your FBI Agent will be proud of this profile picture 👌🏻"]
 
-# On_Message
-call_responses = ["I see you are talking about me, whatsup?", "Oh hey, heard my name so I came here haha, how's it going?","Mind if I join in on the convo?"]
-
 # Shells
 denied = ["exit()","token", "sql_pass", 'eval("token")', "eval('token')",'eval("sql_pass")',"key","cipher","eval('cipher')",'eval("cipher")',"eval('key')",'eval("key")',"reddit_client_id","eval('reddit_client_id')",'eval("reddit_client_id")',"reddit_client_secret","eval('reddit_client_secret')",'eval("reddit_client_secret")',"reddit_user_agent","eval('reddit_user_agent')",'eval("reddit_user_agent")',"reddit_client_id","eval('reddit_client_id')",'eval ("reddit_client_id")',"reddit_user_pass","eval('reddit_user_pass')",'eval("reddit_user_pass")',"reddit_client_id","eval('reddit_client_id')",'eval("reddit_client_id")',"10**10**10","import","exec"]
 denied_responses = ["Uh uh, no can do man. That function will not be executed.","Nope! Can't and won't execute that function","Haha, sorry I cannot possibly execute that function.","That function cannot be executed."]

b76ac175fd4ed3134594b56075a06af1031c0791
TamonudSharma <73441372+spidey711@users.noreply.github.com>
2021-09-28 11:58:55 +0530

    added more responses

--- responses.py
+++ responses.py
@@ -3,11 +3,18 @@ titles = ["Quip Incoming","Here you go!","A quip coming right up!"]
 footers = ["Really interesting if I do say so myself.","Gotta say, this still holds up to today's standards.","Damn, I remember this like it was yesterday.","Holy shit, that was a good!","Man, some of these quips are, in fact, fairly rad!","Did I mention I was present during that time?"]
 
 # Profile Picture
-compliments = ["Man, the daily bugle would pay a lot for this 🤩", "This is a nice one! I like it 😎", "Oh Boy! JJJ is gonna be real happy 😃", "The profile picture is fire 🔥🔥🔥","Great Profile Picture, if I do say so myself 🤔","Damn, where'd you get this? 💙", "Solid 8/10 😎","Beautiful 🌃", "Solid 9/10 😎", "I will consider this an exception and give you 10/10 😎","Your FBI Agent will be proud of this profile picture 👌🏻"]
+compliments = ["Man, the daily bugle would pay a lot for this 🤩", "This is a nice one! I like it 😎", "Oh Boy! JJJ is gonna be real happy 😃", "The profile picture is fire 🔥","Great Profile Picture, if I do say so myself 🤔","Damn, where'd you get this? 💙", "Solid 8/10 😎","Beautiful 🌃", "Solid 9/10 😎", "I will consider this an exception and give you 10/10 😎","Your FBI Agent will be proud of this profile picture 👌🏻"]
 
 # Channels
 poll_channels = ['polls','poll','🔺-polls-🔻']
 announce_channels = ['announcements','bot-announcement','📢-announcements']
+
+# Web Trolls
+troll_links = ["https://s3.getstickerpack.com/storage/uploads/sticker-pack/troll-face-1/sticker_2.png?655b17067df1601e75cc33e267fe806f&d=200x200","https://s3.getstickerpack.com/storage/uploads/sticker-pack/troll-face-1/sticker_3.png?655b17067df1601e75cc33e267fe806f&d=200x200","https://s3.getstickerpack.com/storage/uploads/sticker-pack/troll-face-1/sticker_8.png?655b17067df1601e75cc33e267fe806f&d=200x200","https://s3.getstickerpack.com/storage/uploads/sticker-pack/troll-face-1/sticker_11.png?655b17067df1601e75cc33e267fe806f&d=200x200","https://s3.getstickerpack.com/storage/uploads/sticker-pack/troll-face-1/sticker_23.png?655b17067df1601e75cc33e267fe806f&d=200x200","https://s3.getstickerpack.com/storage/uploads/sticker-pack/troll-face-1/sticker_18.png?655b17067df1601e75cc33e267fe806f&d=200x200","https://s3.getstickerpack.com/storage/uploads/sticker-pack/troll-face-1/sticker_16.png?655b17067df1601e75cc33e267fe806f&d=200x200"]
+
+# Music 
+connections = ["Ready to rock 'n roll, let's go 🎸","Alright then, time to vibe 🌃","So, what do you want to play? 🤔","Man, I am so down for some music right now 🎼","I like trap beats at night and heavy rock 'n roll during the day 🎤"]
+disconnections = ["Well, that was fun 👍🏻","Aight fam, catch you later 👊🏻","Gotta say, very solid song collection 🌟","This has been quite an experience 🎇","Oh hell yeah, that was dope. We should do this more often 😃","Holy Shit, you do have a good taste in music 🥁"]
 
 # Shells
 denied = ["exit()","token", "sql_pass", 'eval("token")', "eval('token')",'eval("sql_pass")',"key","cipher","eval('cipher')",'eval("cipher")',"eval('key')",'eval("key")',"reddit_client_id","eval('reddit_client_id')",'eval("reddit_client_id")',"reddit_client_secret","eval('reddit_client_secret')",'eval("reddit_client_secret")',"reddit_user_agent","eval('reddit_user_agent')",'eval("reddit_user_agent")',"reddit_client_id","eval('reddit_client_id')",'eval ("reddit_client_id")',"reddit_user_pass","eval('reddit_user_pass')",'eval("reddit_user_pass")',"reddit_client_id","eval('reddit_client_id')",'eval("reddit_client_id")',"10**10**10","import","exec"]

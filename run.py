309c32e5452f67fb7625f47c1771a0afa8b62a23
TamonudSharma <73441372+spidey711@users.noreply.github.com>
2021-10-16 00:24:56 +0530

    fixed bugs

--- run.py
+++ run.py
@@ -3,6 +3,5 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv(".env")
-
-if __name__ == "__main":
-    bot.run(os.gentenv('token'))
+if __name__ == "__main__":
+    bot.run(os.getenv('token'))

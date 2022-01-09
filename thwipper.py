e4fa1abaee34b9e05725269d11b94674a5172f96
TamonudSharma <73441372+spidey711@users.noreply.github.com>
2022-01-09 12:07:28 +0530

    new color theme

--- thwipper.py
+++ thwipper.py
@@ -34,7 +34,7 @@ intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix=[
                    prefix for prefix in prefixes], intents=intents, case_insensitive=True)
-color = discord.Color.from_rgb(255, 0, 0)
+color = discord.Color.from_rgb(0, 180, 255)
 bot.remove_command('help')
 # Enviroment Variables
 global auth

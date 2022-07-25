82805a4339ba8dd17e0efd5d31eb125603a4b404
TamonudSharma <73441372+spidey711@users.noreply.github.com>
2022-07-25 23:15:23 +0530

    caught up with changes I've made, been a while

--- thwipper.py
+++ thwipper.py
@@ -184,6 +184,7 @@ def help_menu():
 def time_converter(seconds):
     mins, secs = divmod(seconds, 60)
     hours, mins = divmod(mins, 60)
+    # return "%d hrs %02d mins %02d secs" % (hours, mins, secs)
     if hours == 0: 
         return "%02d mins %02d secs" % (mins, secs)
     if hours > 0: 

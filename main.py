e51f06ddd3a6052a5ccbe6cb286298b9da859a7c
alvinbengeorge <alvinallen333@gmail.com>
2022-08-12 17:49:47 +0530

    🎉 Rewrite

---
+++ main.py
@@ -0,0 +1,25 @@
+import nextcord
+import os
+import regex
+import utils.assets as assets
+
+from dotenv import load_dotenv
+from nextcord.ext import commands, tasks
+
+load_dotenv()
+
+
+
+
+prefixes = ["t!", "_"]
+intents = nextcord.Intents.default()
+intents.members = True
+intents.message_content = True
+bot = commands.Bot(
+    command_prefix=prefixes,
+    intents=intents,
+    case_insensitive=True,
+)
+DEFAULT_COLOR = nextcord.Color.from_rgb(223, 31, 45)
+
+bot.run(os.getenv("token"))
--- run.py
+++
@@ -1,7 +0,0 @@
-from thwipper import bot
-import os
-from dotenv import load_dotenv
-
-load_dotenv(".env")
-if __name__ == "__main__":
-    bot.run(os.getenv("token"))
